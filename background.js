54509d5f84a9b80f9ad75d0ccf9af13c32deb607
Thiago Marinho <eu@thiagomarinho.net>
2024-10-29 22:30:18 -0300

    feat: implement github action detection

--- background.js
+++ background.js
@@ -11,25 +11,69 @@ chrome.runtime.onInstalled.addListener(() => {
 // Handle the context menu click event
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "openWorkflowLink") {
-    const textPattern = /^([a-zA-Z0-9-_]+\/[a-zA-Z0-9-_]+\/.*\.(ya?ml))@([a-zA-Z0-9-_\.\/]+)$/;
-    const match = info.selectionText.match(textPattern);
+    const url = parseReference(info.selectionText);
 
-    if (match) {
-      console.log('URL matched expected github workflow format');
-      const githubWorkflowUrl = parseGithubWorkflowReference(info.selectionText);
-
-      // Open the constructed workflow URL in a new tab
+    if (url) {
       console.log('Opening new tab');
-      chrome.tabs.create({ url: githubWorkflowUrl });
+      chrome.tabs.create({ url: url });
     } else {
-      console.log("URL didn't match expected github workflow format");
-      // If the text doesn't match, show an alert (or handle the error)
-      // chrome.tabs.executeScript(tab.id, {
-      //   code: 'alert("Selected text does not match the required pattern.")'
-      // });
+      console.log(`Failed to build URL from selectedText ${info.selectionText}`);
     }
   }
 });
+
+function parseReference(text) {
+  // Expected format: owner-name/repo-name/path/to/file.yml@github_ref
+  // sample: thiagomarinho/gha-workflow-shortcut/.github/workflows/workflow.yml@main
+  const workflowPattern = /^([a-zA-Z0-9-_]+\/[a-zA-Z0-9-_]+\/.*\.(ya?ml))@([a-zA-Z0-9-_\.\/]+)$/;
+  const isGithubWorkflow = text.match(workflowPattern);
+
+  if (isGithubWorkflow) {
+    console.log('URL matched expected github workflow format');
+    const githubWorkflowUrl = parseGithubWorkflowReference(text);
+
+    if (githubWorkflowUrl) {
+      return githubWorkflowUrl;
+    } else {
+      console.log(`Failed to build URL from workflow reference ${text}`);
+    }
+  } else {
+    console.log("URL didn't match expected github workflow format");
+  }
+
+  // Expected formats: owner-name/repo-name@reference
+  // sample: thiagomarinho/gha-workflow-shortcut@main
+  const actionPattern = /^([a-zA-Z0-9-_]+\/[a-zA-Z0-9-_]+)@([a-zA-Z0-9-_\.\/]+)$/;
+  const isGithubAction = text.match(actionPattern);
+
+  if (isGithubAction) {
+    console.log('URL matched expected github action format');
+    const githubActionUrl = parseGithubActionReference(text);
+
+    if (githubActionUrl) {
+      return githubActionUrl;
+    } else {
+      console.log(`Failed to build URL from action reference ${text}`);
+    }
+  } else {
+    console.log("URL didn't match expected github action format");
+  }
+
+
+
+  return '';
+}
+
+function parseGithubActionReference(text) {
+  const match = text.match(/^([a-zA-Z0-9-_]+\/[a-zA-Z0-9-_]+)@/);
+
+  const githubRef = text.split('@')[1];
+  const url = `https://github.com/${match[1]}/blob/${githubRef}`;
+
+  console.log(`URL: ${url}`);
+
+  return url;
+}
 
 function parseGithubWorkflowReference(text) {
   const splitted = text.split('/');
@@ -46,23 +90,20 @@ function parseGithubWorkflowReference(text) {
   const githubRef = text.split('@')[1];
   const url = `https://github.com/${repository}/blob/${githubRef}/${pathToWorkflow}`;
 
-  console.log(`Github URL: ${url}`);
+  console.log(`URL: ${url}`);
 
   return url;
 }
 
 chrome.omnibox.onInputEntered.addListener((text) => {
-  // Expected format: owner-name/repo-name/path/to/file.yml@github_ref
-  // sample: thiagomarinho/gha-workflow-shortcut/.github/workflows/workflow.yml@main
-
   if (text) {
-    const githubWorkflowUrl = parseGithubWorkflowReference(text);
+    const url = parseReference(text);
 
     // Opening in current tab
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       if (tabs.length > 0) {
         console.log('Updating current tab');
-        chrome.tabs.update(tabs[0].id, { url: githubWorkflowUrl });
+        chrome.tabs.update(tabs[0].id, { url: url });
       }
     });
   }
